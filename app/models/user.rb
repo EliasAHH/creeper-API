135f48b1b859cef0b4625784c3ec8a92098e9a56
EliasAHH <eliasiscoding@gmail.com>
2021-04-22 11:45:05 -0700

    set up match backend

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+    has_secure_password
+    has_many :liked_users, foreign_key: :liker_id, class_name: 'Like'
+    has_many :likes, through: :liked_users
+
+    has_many :likee_users, foreign_key: :likee_id, class_name: 'Like'
+    has_many :likees, through: :likee_users
+end
